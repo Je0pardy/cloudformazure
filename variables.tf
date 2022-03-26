56857e2895f6ad3729be478f02061655ae64dc47
Burak AYDIN <buaydin@outlook.com>
2022-03-26 11:37:44 +0000

    terraform files

---
+++ variables.tf
@@ -0,0 +1,57 @@
+variable "resource_group_name" {
+  type        = string
+  description = "RG name in Azure"
+}
+
+variable "resource_group_location" {
+  type        = string
+  description = "RG location in Azure"
+}
+
+variable "virtual_network_name" {
+  type        = string
+  description = "VNET name in Azure"
+}
+
+variable "subnet_name" {
+  type        = string
+  description = "Subnet name in Azure"
+}
+
+variable "public_ip_name" {
+  type        = string
+  description = "Public IP name in Azure"
+}
+
+variable "network_security_group_name" {
+  type        = string
+  description = "NSG name in Azure"
+}
+
+variable "network_interface_name" {
+  type        = string
+  description = "NIC name in Azure"
+}
+
+variable "linux_virtual_machine_name" {
+  type        = string
+  description = "Linux VM name in Azure"
+}
+
+variable "linux_vm_image_publisher" {
+  type        = string
+  description = "Virtual machine source image publisher"
+  default     = "Canonical"
+}
+
+variable "linux_vm_image_offer" {
+  type        = string
+  description = "Virtual machine source image offer"
+  default     = "0001-com-ubuntu-server-focal"
+}
+
+variable "ubuntu_2004_gen2_sku" {
+  type        = string
+  description = "SKU for Ubuntu 20.04 LTS Gen2"
+  default     = "20_04-lts-gen2"
+}
