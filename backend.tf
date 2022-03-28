0297d2ddce19606b0ccfbcc1a142bc18d6f1b35c
Je0pardy <96169875+Je0pardy@users.noreply.github.com>
2022-03-28 16:57:22 +0300

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
-   backend "azurerm" {
-      resource_group_name  = "cloud-shell-storage-tfstate"
-      storage_account_name = "buaydinsam"
-      container_name       = "tfstate"
-      key                  = "terraform.tfstate"
-    }
+  backend "azurerm" {
+    resource_group_name  = "cloud-shell-storage-tfstate"
+    storage_account_name = "buaydinsam"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
 }
